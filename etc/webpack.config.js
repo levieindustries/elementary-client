dd895c3c6b65039dfe6a9bf2b354efb1b57cbd78
Lars Levie <larslevie@gmail.com>
2017-11-11 10:17:14 -0600

    Remove stage-0

--- etc/webpack.config.js
+++ etc/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/env", "stage-0", "react"]
+          presets: ["env", "react"]
         }
       },
       {
