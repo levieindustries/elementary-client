78f44f348c8aa966c68862713f77959995f7f3aa
Lars Levie <larslevie@gmail.com>
2017-11-11 09:57:01 -0600

    Config babel per docs

--- etc/webpack.config.js
+++ etc/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
-          presets: ["env", "stage-0", "react"]
+          presets: ["@babel/env", "stage-0", "react"]
         }
       },
       {
